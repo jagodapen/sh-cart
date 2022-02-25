06234cca70ee03708209057320e6ef06e660e8d6
zlotawanna@gmail.com <zlotawanna@gmail.com>
2022-02-25 15:29:17 +0100

    create simple layout

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,8 @@
 class HomeController < ApplicationController
   def index
+    @cards = {"Lists": "shopping-lists.svg",
+          "Plans": "week-plans.svg",
+          "Products": "fav-products.svg",
+          "Recipes": "fav-recipes.svg"}
   end
 end
