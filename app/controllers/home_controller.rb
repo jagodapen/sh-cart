5865ba0aeb6f4541db7b4db219534adc052f4a58
zlotawanna@gmail.com <zlotawanna@gmail.com>
2022-03-03 20:31:48 +0100

    layout update - code clean up

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,8 +1,6 @@
 class HomeController < ApplicationController
+
   def index
-    @cards = {"lists": "shopping-lists.svg",
-              "plans": "week-plans.svg",
-              "products": "fav-products.svg",
-              "recipes": "fav-recipes.svg"}
   end
+
 end
