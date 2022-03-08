3d17f5921021705af824954e7813f36c311883fe
zlotawanna@gmail.com <zlotawanna@gmail.com>
2022-03-08 14:11:53 +0100

    add produc types column to products

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,6 @@
 class Product < ApplicationRecord
   validates :name, presence: true, length: { minimum: 2, maximum: 50 }
   validates :unit, presence: true, length: { maximum: 15 }
+
+  enum :product_type, [ :fruit, :vegetable, :nuts, :meat, :egg, :seafood, :dairy_products, :cereal_products, :spices, :extras, :fat, :drink, :other ]
 end
