a6881150161691ec6f21c70409ecf9736c482225
zlotawanna@gmail.com <zlotawanna@gmail.com>
2022-03-09 12:46:55 +0100

    update layout of products

--- app/helpers/products_helper.rb
+++ app/helpers/products_helper.rb
@@ -1,2 +1,11 @@
 module ProductsHelper
+
+  def product_type_haml_id(name)
+    @product_type_haml_id = "product_type_" + name
+  end
+
+  def products_of_type(name)
+    @products_of_type = Product.where(product_type: name)
+  end
+
 end
