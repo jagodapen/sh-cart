317790705218c1ea050206c75821b0169b69f85e
zlotawanna@gmail.com <zlotawanna@gmail.com>
2022-03-14 19:58:02 +0100

    products index page - code refactoring - group_by

--- app/helpers/products_helper.rb
+++ app/helpers/products_helper.rb
@@ -1,11 +1,2 @@
 module ProductsHelper
-
-  def product_type_haml_id(name)
-    @product_type_haml_id = "product_type_" + name
-  end
-
-  def products_of_type(name)
-    @products_of_type = Product.where(product_type: name)
-  end
-
 end
