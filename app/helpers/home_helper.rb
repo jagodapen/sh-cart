dc01a30dd59b0a7a09e937f652fbf7408832f4a7
zlotawanna@gmail.com <zlotawanna@gmail.com>
2022-03-15 21:36:22 +0100

    shopping-lists layouts update; error message partial adjustment

--- app/helpers/home_helper.rb
+++ app/helpers/home_helper.rb
@@ -1,7 +1,7 @@
 module HomeHelper
-  
+
   def home_cards
-    @home_cards = {"lists": "shopping-lists.svg",
+    @home_cards = {"shopping_lists": "shopping-lists.svg",
                   "plans": "week-plans.svg",
                   "products": "fav-products.svg",
                   "recipes": "fav-recipes.svg"}
