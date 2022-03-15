03cb0a69b30baec061302cdadf447c1b076838a9
zlotawanna@gmail.com <zlotawanna@gmail.com>
2022-03-15 15:57:27 +0100

    create shopping list table

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,4 +1,10 @@
 FactoryBot.define do
+  factory :shopping_list do
+    name { "MyString" }
+    shopping_day { "2022-03-15" }
+    status { 1 }
+  end
+
   factory :product do
     name { Faker::Food.ingredient }
     unit { Faker::Food.metric_measurement }
