7e873ac27a946e492965faddbe1381c15300f0fc
zlotawanna@gmail.com <zlotawanna@gmail.com>
2022-03-17 13:59:54 +0100

    create recipe table

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_03_15_204157) do
-
+ActiveRecord::Schema[7.0].define(version: 2022_03_17_125536) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -21,6 +20,14 @@ ActiveRecord::Schema[7.0].define(version: 2022_03_15_204157) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "product_type"
+  end
+
+  create_table "recipes", force: :cascade do |t|
+    t.string "name"
+    t.text "description"
+    t.time "preparation_time"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "shopping_lists", force: :cascade do |t|
