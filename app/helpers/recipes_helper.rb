53dcfd38173192326398759e78b3362a1979677e
zlotawanna@gmail.com <zlotawanna@gmail.com>
2022-03-18 13:03:22 +0100

    recipes preparation time - update layouts and database

--- app/helpers/recipes_helper.rb
+++ app/helpers/recipes_helper.rb
@@ -1,2 +1,9 @@
 module RecipesHelper
+
+  def preparation_time_hours(minutes)
+    min = (minutes % 60).to_s + "min"
+    hours = (minutes/60).floor().to_s + "h "
+    @preparation_time_hours = minutes>59 ? hours + min : min
+  end
+
 end
