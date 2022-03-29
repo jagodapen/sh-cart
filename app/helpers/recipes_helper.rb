5b8d0053b9f379dfca40d923fdb988a54abd8cc7
zlotawanna@gmail.com <zlotawanna@gmail.com>
2022-03-29 15:09:28 +0200

    recipes - clean up the code

--- app/helpers/recipes_helper.rb
+++ app/helpers/recipes_helper.rb
@@ -2,20 +2,20 @@ module RecipesHelper
 
   def preparation_time_hours(minutes)
     min = (minutes % 60).to_s + "min"
-    hours = (minutes/60).floor().to_s + "h "
-    @preparation_time_hours = minutes>59 ? hours + min : min
+    hours = (minutes/60).floor.to_s + "h "
+    minutes>59 ? hours + min : min
   end
 
   def recipe_product(id)
-    @recipe_product = Product.find(id)
+    Product.find(id)
   end
 
   def all_products
-    @all_products = Product.all
+    Product.all
   end
 
   def grouped_products
-    @grouped_products = Product.all.group_by(&:product_type)
+    Product.all.group_by(&:product_type)
   end
 
 end
