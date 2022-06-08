bf4e558a6e16702dd6198d50a18985dbb02f4953
zlotawanna@gmail.com <zlotawanna@gmail.com>
2022-06-08 20:21:01 +0200

    products api endpoint - routes edit to be more RESTful

--- config/routes.rb
+++ config/routes.rb
@@ -6,8 +6,7 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   namespace :api do
     namespace :v1 do
-      # resources :products, only: [:index]
-      get '/products', to: 'products#index'
+      resources :products, only: [:index]
     end
   end
 end
