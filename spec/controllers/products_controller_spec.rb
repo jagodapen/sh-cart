4484e9917938d904b9ba7398038f238c5ee189bd
Jagoda Pendolska <zlotawanna@gmail.com>
2023-05-09 13:11:56 +0200

    Fix lint

--- spec/controllers/products_controller_spec.rb
+++ spec/controllers/products_controller_spec.rb
@@ -2,85 +2,85 @@ require 'rails_helper'
 
 RSpec.describe Product, type: :request do
 
-    describe 'GET #index' do
-        it 'shows all products' do
-            product1 = create :product
-            product2 = create :product
-            
-            get products_path
+  describe 'GET #index' do
+    it 'shows all products' do
+      product1 = create :product
+      product2 = create :product
+      
+      get products_path
 
-            expect(response).to be_successful
-            expect(response.body).to include(product1.name, product1.unit, product2.name, product2.unit)
-        end
+      expect(response).to be_successful
+      expect(response.body).to include(product1.name, product1.unit, product2.name, product2.unit)
     end
+  end
 
-    describe 'GET #new' do
-        it 'renders form' do
-            get new_product_path
+  describe 'GET #new' do
+    it 'renders form' do
+      get new_product_path
 
-            expect(response).to be_successful
-        end
+      expect(response).to be_successful
     end
+  end
 
-    describe 'GET #edit' do
-        it 'renders form' do
-            product = create :product
+  describe 'GET #edit' do
+    it 'renders form' do
+      product = create :product
 
-            get edit_product_path(product)
+      get edit_product_path(product)
 
-            expect(response).to be_successful
-            expect(response.body).to include(product.name, product.unit)
-        end
+      expect(response).to be_successful
+      expect(response.body).to include(product.name, product.unit)
     end
+  end
 
-    describe 'GET #show' do
-        it 'shows product' do
-            product = create :product
+  describe 'GET #show' do
+    it 'shows product' do
+      product = create :product
 
-            get product_path(product)
-            
-            expect(response).to be_successful
-            expect(response.body).to include(product.name, product.unit)
-        end
+      get product_path(product)
+      
+      expect(response).to be_successful
+      expect(response.body).to include(product.name, product.unit)
     end
+  end
 
-    describe 'POST #create' do
-        it 'creates new product' do
-            params = { 
-                product: { 
-                            name: Faker::Food.ingredient, 
-                            unit: Faker::Food.metric_measurement, 
-                            product_type: 'fruit' 
-                        } 
-            }
+  describe 'POST #create' do
+    it 'creates new product' do
+      params = { 
+          product: { 
+                      name: Faker::Food.ingredient, 
+                      unit: Faker::Food.metric_measurement, 
+                      product_type: 'fruit' 
+                  } 
+      }
 
-            post products_path, params: params
+      post products_path, params: params
 
-            expect(response).to redirect_to(product_path(Product.first))
-            expect(Product.all.count).to eq 1
-        end
+      expect(response).to redirect_to(product_path(Product.first))
+      expect(Product.all.count).to eq 1
     end
+  end
 
-    describe 'PUT #update' do
-        it 'updates product' do
-            product = create :product
-            new_params = { product: { name: 'New name', unit: 'New unit', product_type: 'fruit' } }
+  describe 'PUT #update' do
+    it 'updates product' do
+      product = create :product
+      new_params = { product: { name: 'New name', unit: 'New unit', product_type: 'fruit' } }
 
-            put product_path(product), params: new_params
-            
-            expect(response).to redirect_to(product_path(product))
-            expect(product.reload.name).to eq 'New name'
-        end
+      put product_path(product), params: new_params
+      
+      expect(response).to redirect_to(product_path(product))
+      expect(product.reload.name).to eq 'New name'
     end
+  end
 
-    describe 'DELETE #destroy' do
-        it 'destroy product' do
-            product = create :product
+  describe 'DELETE #destroy' do
+    it 'destroy product' do
+      product = create :product
 
-            delete product_path(product)
-            
-            expect(response).to redirect_to(products_path)
-            expect(Product.all.count).to eq 0
-        end
+      delete product_path(product)
+      
+      expect(response).to redirect_to(products_path)
+      expect(Product.all.count).to eq 0
     end
+  end
 end
