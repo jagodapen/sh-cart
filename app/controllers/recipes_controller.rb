6ffc80fe615ab9d16dd9f280085b1b35bf1a100c
Jagoda Pendolska <zlotawanna@gmail.com>
2023-05-12 13:12:07 +0200

    Create Recipe Repository and use it in controller; Add controller tests

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -3,77 +3,64 @@
 class RecipesController < ApplicationController
   before_action :set_recipe, only: %i(show edit update destroy)
 
-  # GET /recipes or /recipes.json
   def index
-    @recipes = Recipe.all
+    @recipes = repository.all
   end
 
-  # GET /recipes/1 or /recipes/1.json
-  def show
-    @recipe_products = RecipeProduct.where(recipe_id: @recipe)
-  end
+  def show; end
 
-  # GET /recipes/new
   def new
-    @recipe = Recipe.new
-    5.times { @recipe.recipe_products.build }
-    @recipe_product = RecipeProduct.new
+    @recipe = repository.new_entity
+    build_recipe_product
   end
 
-  # GET /recipes/1/edit
   def edit
-    @recipe_product = RecipeProduct.new
+    build_recipe_product
   end
 
-  # POST /recipes or /recipes.json
   def create
-    @recipe = Recipe.new(recipe_params)
+    @recipe = repository.new_entity(attrs: recipe_params)
 
-    respond_to do |format|
-      if @recipe.save
-        format.html { redirect_to recipe_url(@recipe), notice: "Recipe was successfully created." }
-        format.json { render :show, status: :created, location: @recipe }
-      else
-        format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @recipe.errors, status: :unprocessable_entity }
-      end
+    if repository.save(@recipe)
+      redirect_to recipe_url(@recipe), notice: "Recipe was successfully created."
+    else
+      render :new, status: :unprocessable_entity
     end
   end
 
-  # PATCH/PUT /recipes/1 or /recipes/1.json
   def update
-    respond_to do |format|
-      if @recipe.update(recipe_params)
-        format.html { redirect_to recipe_url(@recipe), notice: "Recipe was successfully updated." }
-        format.json { render :show, status: :ok, location: @recipe }
-      else
-        format.html { render :edit, status: :unprocessable_entity }
-        format.json { render json: @recipe.errors, status: :unprocessable_entity }
-      end
+    @recipe.attributes = recipe_params
+
+    if repository.save(@recipe)
+      redirect_to recipe_url(@recipe), notice: "Recipe was successfully updated."
+    else
+      render :edit, status: :unprocessable_entity
     end
   end
 
-  # DELETE /recipes/1 or /recipes/1.json
   def destroy
-    @recipe.destroy
+    repository.delete(@recipe)
 
-    respond_to do |format|
-      format.html { redirect_to recipes_url, notice: "Recipe was successfully destroyed." }
-      format.json { head :no_content }
-    end
+    redirect_to recipes_url, notice: "Recipe was successfully destroyed."
   end
 
   private
 
-  # Use callbacks to share common setup or constraints between actions.
-  def set_recipe
-    @recipe = Recipe.find(params[:id])
+  def repository
+    RecipeRepository.new
   end
 
-  # Only allow a list of trusted parameters through.
+  def set_recipe
+    @recipe = repository.find(id: params[:id])
+  end
+
   def recipe_params
     params.require(:recipe).permit(:name, :description, :preparation_time,
                                    recipe_products_attributes:
                                    %i(id recipe_id product_id quantity calories _destroy))
+  end
+
+  def build_recipe_product
+    @recipe_product = @recipe.recipe_products.build
   end
 end
---
+++ spec/controllers/recipes_controller_spec.rb
@@ -0,0 +1,124 @@
+# frozen_string_literal: true
+
+require "rails_helper"
+
+RSpec.describe Recipe, type: :request do
+  describe "GET #index" do
+    it "shows all recipes" do
+      recipe1 = create(:recipe, preparation_time: 20)
+      recipe2 = create(:recipe, preparation_time: 30)
+
+      get recipes_path
+
+      expect(response).to be_successful
+      expect(response.body).to include(
+        *[
+          recipe1.name,
+          recipe1.preparation_time.to_s,
+          recipe2.name,
+          recipe2.preparation_time.to_s,
+        ]
+      )
+    end
+  end
+
+  describe "GET #new" do
+    it "renders form" do
+      get new_recipe_path
+
+      expect(response).to be_successful
+    end
+  end
+
+  describe "GET #edit" do
+    it "renders form" do
+      recipe = create(:recipe)
+
+      get edit_recipe_path(recipe)
+
+      expect(response).to be_successful
+      expect(response.body).to include(
+        *[
+          recipe.name, 
+          recipe.preparation_time.to_s,
+          recipe.description,
+        ]
+      )
+    end
+  end
+
+  describe "GET #show" do
+    it "shows recipe" do
+      recipe = create(:recipe, preparation_time: 20)
+
+      get recipe_path(recipe)
+
+      expect(response).to be_successful
+      expect(response.body).to include(
+        *[
+          recipe.name, 
+          recipe.preparation_time.to_s,
+          recipe.description,
+        ]
+      )
+    end
+  end
+
+  describe "POST #create" do
+    it "creates new recipe" do
+      product1 = create(:product)
+      product2 = create(:product)
+      params = {
+        recipe: {
+          name: "Recipe 1", 
+          descritpion: "some description", 
+          preparation_time: 100,
+          recipe_products_attributes: {
+            "1": { product_id: product1.id, quantity: 1 },
+            "2": { product_id: product2.id, quantity: 2 },
+          },
+        }
+      }
+
+      post recipes_path, params: params
+
+      expect(response).to redirect_to(recipe_path(described_class.last))
+      expect(described_class.all.count).to eq 1
+      expect(described_class.last.products.count).to eq 2
+    end
+  end
+
+  describe "PUT #update" do
+    it "updates recipe" do
+      recipe = create(:recipe)
+      product = create(:product)
+      new_params = {
+        recipe: {
+          name: "new recipe name", 
+          descritpion: "some description", 
+          preparation_time: 100,
+          recipe_products_attributes: {
+            "1": { product_id: product.id, quantity: 1 }
+          },
+        }
+      }
+
+      put recipe_path(recipe), params: new_params
+
+      expect(response).to redirect_to(recipe_path(recipe))
+      expect(recipe.reload.name).to eq "new recipe name"
+      expect(described_class.last.products.count).to eq 1
+    end
+  end
+
+  describe "DELETE #destroy" do
+    it "destroy recipe" do
+      recipe = create(:recipe)
+
+      delete recipe_path(recipe)
+
+      expect(response).to redirect_to(recipes_path)
+      expect(described_class.all.count).to eq 0
+    end
+  end
+end
