c45b64840bd7be23bd0cc23df20af321c767614a
Jagoda Pendolska <zlotawanna@gmail.com>
2023-05-13 20:13:49 +0200

    Move calories calculation service to use_cases folder; fix naming in tests

---
+++ app/services/recipes/use_cases/calculate_recipe_products_calories.rb
@@ -0,0 +1,27 @@
+# frozen_string_literal: true
+
+module Recipes
+  module UseCases
+    class CalculateRecipeProductsCalories
+      def initialize(recipe)
+        @recipe = recipe
+      end
+
+      def call
+        calculate_recipe_products_calories
+      end
+
+      private
+
+      def calculate_recipe_products_calories
+        @recipe.recipe_products.each do |rp|
+          next unless rp.product_calories
+
+          calories_of_one = rp.product_calories&.calories
+          calculated_calories = calories_of_one * rp.quantity
+          rp.update(calories: cśalculated_calories)
+        end
+      end
+    end
+  end
+end
