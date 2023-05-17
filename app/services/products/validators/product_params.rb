c77029b23a726e206d65572b6c70c7fb739aedae
Jagoda Pendolska <zlotawanna@gmail.com>
2023-05-17 11:33:28 +0200

    Update validator

--- app/services/products/validators/product_params.rb
+++ app/services/products/validators/product_params.rb
@@ -5,7 +5,11 @@ module Products
     class ProductParams < ::Dry::Validation::Contract
       params do
         required(:name).filled.value(:str?)
-        required(:product_type).filled.value(:str?, included_in?: Product::PRODUCT_TYPES)
+        required(:product_type).filled.value(included_in?: Product::PRODUCT_TYPES)
+      end
+
+      rule(:name) do
+        key.failure("must consist of letters") unless value.to_i.zero? && value != "0"
       end
     end
   end
